d48243b142c2dc2d66a1a2a21da467474c41e868
Zebus <zebusjesus@pm.me>
2021-07-11 20:29:17 -0700

    cleanup

--- bin/run.sh
+++
@@ -1,2 +0,0 @@
-# A little wrapper so we can test run the app without doing pip3 install
-python3 -m meshtastic-pygui "$@"
--- bin/upload-release.sh
+++
@@ -1,5 +0,0 @@
-rm dist/*
-set -e
-
-python3 setup.py sdist bdist_wheel
-python3 -m twine upload dist/*
